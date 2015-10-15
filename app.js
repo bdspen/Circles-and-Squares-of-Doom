bfe4a69fccf8205c3d50031c328ee908a49a25e5
Ben <bdspen@gmail.com>
2015-10-15 14:44:31 -0700

    add angular router and route for 'game', create game.html, edit index and negligible edit to controller

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var circlesSquares = angular.module('circlesSquares',[]);
+var circlesSquares = angular.module('circlesSquares',['ui.router']);
+
+circlesSquares.config(function($stateProvider, $urlRouterProvider){
+
+  //make welcome page route
+
+  //route to game
+  $stateProvider.state('game', {
+    url:"",
+    templateUrl: "partials/game.html",
+    controller: "BoardCtrl"
+
+  });
+});
--- scripts.js
+++ scripts.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
   $(".timer").TimeCircles().stop();
+
   $("#timerStart").click(function(){
     $(".timer").TimeCircles().start();
   });
-
-  var timerValue = function() {
-    while(i <= 31) {
-      $(".timerValue").text();
-      console.log(timerValue);
-
-    }
-  }
 });
